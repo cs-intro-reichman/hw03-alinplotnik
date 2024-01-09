ac91933ba554fdbba6f9b5fdc2eda02431fed944
alinplotnik <alin.plotnikov@runi.ac.il>
2024-01-09 20:16:52 +0200

    Home Work 03

--- Calendar.java
+++ Calendar.java
@@ -1,7 +1,5 @@
-/** 
- * Prints the calendars of all the years in the 20th century.
- */
-public class Calendar1 {	
+
+public class Calendar {	
     // Starting the calendar on 1/1/1900
 	static int dayOfMonth = 1;   
 	static int month = 1;
@@ -9,10 +7,6 @@ public class Calendar1 {
 	static int dayOfWeek = 2;     // 1.1.1900 was a Monday
 	static int nDaysInMonth = 31; // Number of days in January
 	
-	/** 
-	 * Prints the calendars of all the years in the 20th century. Also prints the  
-	 * number of Sundays that occured on the first day of the month during this period.
-	 */
 	public static void main(String args[]) {
         int wantedYear = Integer.parseInt(args[0]);
 	    int debugDaysCounter = 0; 
@@ -56,15 +50,11 @@ public class Calendar1 {
         }
     } 
 		 
-    // Returns true if the given year is a leap year, false otherwise.
+  
 	public static boolean isLeapYear(int year) {
 		return ((year % 400) == 0) || (((year % 4) == 0) && ((year % 100) != 0));
 	}
-	 
-	// Returns the number of days in the given month and year.
-	// April, June, September, and November have 30 days each.
-	// February has 28 days in a common year, and 29 days in a leap year.
-	// All the other months have 31 days.
+	
 	public static int nDaysInMonth(int month, int year) {
 		if (month == 4 || month == 6 || month == 9 || month == 11) {
             return 30;
